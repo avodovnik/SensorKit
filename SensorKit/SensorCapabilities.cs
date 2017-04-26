a8e98b115332579dedb3c3ac5b63d8a8ebf02fa6
Kevin Ashley <kevinash@outlook.com>
2017-04-26 08:57:10 -0700

    initial commit

---
+++ SensorKit/SensorCapabilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorKit
+{
+    [Flags]
+    public enum SensorCapabilities : int
+    {
+        Hub, // sensor hub device, e.g. mobile device
+        IMU,
+        ChangeColor,
+        Button,
+        Mute
+    }
+}
---
+++ SensorKit/SensorItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorKit
+{
+    [DataContract]
+    public class SensorItem
+    {
+        public DateTimeOffset timestamp { get; set; }
+
+        public int activityTypeId { get; set; }
+
+        public double aX { get; set; }
+        public double aY { get; set; }
+        public double aZ { get; set; }
+        public double avX { get; set; }
+        public double avY { get; set; }
+        public double avZ { get; set; }
+        public double qW { get; set; }
+        public double qX { get; set; }
+        public double qY { get; set; }
+        public double qZ { get; set; }
+
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public double speed { get; set; }
+        public double alt { get; set; }
+        public double incl { get; set; }
+    }
+}
